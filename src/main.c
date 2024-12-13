00aca980a2ddec730f19ac1e08df018109e68562
Arhab Sofiane <arhabsofiane123@gmail.com>
2024-12-13 19:51:28 +0100

    Misc changes

--- src/main.c
+++ src/main.c
@@ -10,7 +10,7 @@
 #include <stdio.h>
 
 static char* DEFAULT_CONFIG_PATH = "../config.txt";
-static char* DEFAULT_OUTPUT_PATH = "../scripts/result.txt";
+static char* DEFAULT_OUTPUT_PATH = "../results/result.txt";
 
 static void save_results(
     FILE ofp[static 1],
